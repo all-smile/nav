428d80bd8b8b826a29fa76010fb646a92ca48dc8
肖俊涛 <allblue95@126.com>
2022-08-22 14:49:13 +0800

    add: cnblog 侧边公告js文件

---
+++ static/cnblog/sidebar.js
@@ -0,0 +1,125 @@
+<script type="text/javascript">
+    window.cnblogsConfig = {
+      // iconfont
+      fontIconExtend: "//at.alicdn.com/t/c/font_3603036_keyavmcqbhn.css",
+      info: {
+        name: '甜点cc', // 用户名
+        startDate: '2016-10-08', // 入园时间，年-月-日。入园时间查看方法：鼠标停留园龄时间上，会显示入园时间
+        avatar: 'https://pic.cnblogs.com/avatar/1037867/20220801164532.png', // 用户头像
+        blogIcon: 'https://pic.cnblogs.com/avatar/1037867/20220801164532.png', // 网站图标 favicon.ico
+      },
+      sidebar: {
+        infoBackground: 'https://cdn.jsdelivr.net/gh/all-smile/nav@master/static/images/Machine_tool.jpg', // 个人信息背景
+        titleMsg: '宝宝等你很久啦~🌹🌹', // 侧边栏title信息
+        navList: [
+            ['友链', 'https://www.cnblogs.com/all-smile/p/16612170.html', 'icon-pengyoufill'],
+            ['个人主页', 'https://home.i-xiao.space/', 'icon-zhuye'],
+        ],
+        // customList: {
+        //   "title1": { // 标题
+        //       "data": [ // 列表数据 ['列表', '链接']
+        //           ['我的博客1', 'https://www.cnblogs.com/bndong/'],
+        //       ],
+        //       "icon": "icon-brush_fill" // 配置图标，参考文档：定制化/字体图标库
+        //   },
+        // }
+      },
+      banner: {
+        home: {
+          // 主页banner标语, 随机从数组中获取一条文字显示
+          title: ['每一个不曾起舞的日子，都是对生命的辜负。', '为者常成，行者常至。', '逝者如斯夫，不舍昼夜。', "相逢的还会再相逢。"],
+          // 主页banner图片
+          background: [
+              "https://cdn.jsdelivr.net/gh/all-smile/nav@master/static/images/Machine_tool.jpg",
+              "https://cdn.jsdelivr.net/gh/all-smile/nav@master/static/images/horse.jpg",
+              "https://cdn.jsdelivr.net/gh/all-smile/nav@master/static/images/background.jpg",
+              "https://raw.githubusercontent.com/all-smile/nav/master/static/images/sunset.jpg",
+              "https://raw.githubusercontent.com/all-smile/nav/master/static/images/sunset02.jpg"
+          ],
+        },
+      },
+      // 失去焦点标签文字
+      title: {
+        onblur: '(oﾟvﾟ)ノ Hi',
+      },
+      // 页面加载loading。
+      loading: {
+        rebound: {
+          tension: 16,
+        },
+        spinner: {
+          id: 'spinner',
+          radius: 90,
+        }
+      },
+      // 页脚模式
+      footer: {
+        style: 2,
+        text: {
+          left: '蓄之既久，其发必速。',
+        },
+      },
+      // 友情链接
+      links: {
+        footer: [
+            /*
+            格式：
+            Key	Description
+            name	昵称
+            introduction	简介
+            avatar	头像
+            url	友链地址
+            */
+            ["添加友链", 'https://msg.cnblogs.com/send/甜点cc'],
+        ],
+      },
+      // 右下角菜单
+      rtMenu: {
+        qrCode: 'https://cdn.jsdelivr.net/gh/all-smile/nav@master/static/images/WeChat.jpg',
+        reward: {
+          wechatpay: 'https://cdn.jsdelivr.net/gh/all-smile/nav@master/static/images/buymeacoffee.jpg'
+        },
+      },
+      animate: {
+        // 主页banner动效配置。
+        homeBanner: {
+          enable: true,
+          options: {
+            radius: 15,
+          }
+        },
+        // 文章页标题动效配置。
+        articleTitle: {
+          enable: true,
+        },
+        // 文章页banner动效配置
+        articleBanner: {
+          enable: true,
+        },
+        // 背景动效。
+        background: {
+          enable: false,
+        },
+        // 跟随鼠标动效。
+        mouse: {
+          enable: false,
+        },
+      },
+      // articleSuffix: {
+      //   copyrightHtml: "版权...",
+      // },
+    };
+    // 友链配置
+    window.cnblogsConfig.links.page = [
+      {
+        name: '甜点cc', // 昵称
+        introduction: '博客', // 简介
+        avatar: 'https://pic.cnblogs.com/avatar/1037867/20220801164532.png', // 头像
+        url: 'https://home.i-xiao.space/blog/' // 友链地址
+      },
+    ]
+</script>
+<script src="https://cdn.jsdelivr.net/gh/BNDong/Cnblogs-Theme-SimpleMemory@v2.1.0/dist/simpleMemory.js" defer></script>
+<!-- 下面的采用个人托管方式报错 -->
+<!-- <script src="https://cdn.jsdelivr.net/gh/all-smile/nav@1.0.0/static/cnblog/simpleMemory.js" defer></script> -->
+<!-- <script src="https://blog-static.cnblogs.com/files/blogs/342390/simpleMemory.js" defer></script> -->
